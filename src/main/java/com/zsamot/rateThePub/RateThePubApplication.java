1c995779575df35cec549c447c220b4740d3bea8
Tomasz <tomasz.chudek@onet.pl>
2023-02-21 22:52:34 +0100

    Init

---
+++ src/main/java/com/zsamot/rateThePub/RateThePubApplication.java
@@ -0,0 +1,13 @@
+package com.zsamot.rateThePub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateThePubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateThePubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zsamot/rateThePub/RateThePubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsamot.rateThePub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateThePubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
